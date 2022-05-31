321a73f8b840ebb8f303cc842367b0da612b0ec2
Harshavardhan Unnibhavi <harshanavkis@gmail.com>
2022-05-31 14:55:28 +0000

    retab: fix ugly indentation issues in default.nix
    Signed-off-by: Harshavardhan Unnibhavi <harshanavkis@gmail.com>

--- default.nix
+++ default.nix
@@ -5,90 +5,90 @@ let
 
   lizard = buildPythonPackage rec{
     name = "lizard";
-	version = "1.17.9";
+    version = "1.17.9";
 
-	src = pkgs.fetchurl {
-	  url = "https://github.com/terryyin/lizard/archive/refs/tags/${version}.tar.gz";
-	  sha256 = "22559181a556bdda2577420f4f6a54428109b0af9b832d77f08fcaf26b4019e3";
+    src = pkgs.fetchurl {
+      url = "https://github.com/terryyin/lizard/archive/refs/tags/${version}.tar.gz";
+      sha256 = "22559181a556bdda2577420f4f6a54428109b0af9b832d77f08fcaf26b4019e3";
     };
 
-	doCheck = false;
+    doCheck = false;
   };
 
   pydriller = buildPythonPackage rec{
     name = "pydriller";
-	version = "2.1";
+    version = "2.1";
 
-	src = pkgs.fetchurl {
-	  url = "https://github.com/ishepard/pydriller/archive/refs/tags/${version}.tar.gz";
-	  sha256 = "e110d14a5d9e8eebcaf8c9a8e16820e0bab19d49c10b7fb5393f4a18dc6bdac5";
+    src = pkgs.fetchurl {
+      url = "https://github.com/ishepard/pydriller/archive/refs/tags/${version}.tar.gz";
+      sha256 = "e110d14a5d9e8eebcaf8c9a8e16820e0bab19d49c10b7fb5393f4a18dc6bdac5";
     };
 
-	buildInputs = with self; [ lizard pytz GitPython types-pytz ];
+    buildInputs = with self; [ lizard pytz GitPython types-pytz ];
 
-	doCheck = false;
+    doCheck = false;
   };
 
   meson = buildPythonPackage rec{
     name = "meson";
-	version = "0.57.1";
+    version = "0.57.1";
 
-	src = pkgs.fetchurl {
-	  url = "https://github.com/mesonbuild/meson/releases/download/${version}/meson-${version}.tar.gz";
-	  sha256 = "sha256-cuHHgrqb2iBPSh7Vf5jQJ9e265QUxyPuu9bsfxlVyKY=";
+    src = pkgs.fetchurl {
+      url = "https://github.com/mesonbuild/meson/releases/download/${version}/meson-${version}.tar.gz";
+      sha256 = "sha256-cuHHgrqb2iBPSh7Vf5jQJ9e265QUxyPuu9bsfxlVyKY=";
     };
 
-	doCheck = false;
+    doCheck = false;
   };
 
   markupsafe = buildPythonPackage rec{
     name = "markupsafe";
-	version = "2.0.1";
+    version = "2.0.1";
 
-	src = pkgs.fetchurl {
-	  url = "https://github.com/pallets/markupsafe/archive/refs/tags/2.0.1.tar.gz";
-	  sha256 = "sha256-JJowJouuXAmC7k8xq7cSZXfANgIJKg8sZLBqT2gXazA=";
+    src = pkgs.fetchurl {
+      url = "https://github.com/pallets/markupsafe/archive/refs/tags/2.0.1.tar.gz";
+      sha256 = "sha256-JJowJouuXAmC7k8xq7cSZXfANgIJKg8sZLBqT2gXazA=";
     };
 
-	doCheck = false;
+    doCheck = false;
   };
 
   jinja2 = buildPythonPackage rec{
     name = "jinja2";
-	version = "2.11.3";
+    version = "2.11.3";
 
-	src = pkgs.fetchurl {
-	  url = "https://github.com/pallets/jinja/archive/refs/tags/${version}.tar.gz";
-	  sha256 = "sha256-S+RKnlX7BpNHshfS1m1R2LCpfy92mFuQYwu08xfrBCo=";
+    src = pkgs.fetchurl {
+      url = "https://github.com/pallets/jinja/archive/refs/tags/${version}.tar.gz";
+      sha256 = "sha256-S+RKnlX7BpNHshfS1m1R2LCpfy92mFuQYwu08xfrBCo=";
     };
 
-	buildInputs = with self; [ markupsafe ];
+    buildInputs = with self; [ markupsafe ];
   };
 
   vunit_hdl = buildPythonPackage rec{
     name = "vunit_hdl";
-	version = "4.6.0";
+    version = "4.6.0";
 
-	src = pkgs.fetchurl {
-	  url = "https://github.com/VUnit/vunit/archive/refs/tags/v${version}.tar.gz";
-	  sha256 = "sha256-a601b5XfmtPaXoiWtFeQLC8xMe/0LbpuyLDLwmbTwBk=";
+    src = pkgs.fetchurl {
+      url = "https://github.com/VUnit/vunit/archive/refs/tags/v${version}.tar.gz";
+      sha256 = "sha256-a601b5XfmtPaXoiWtFeQLC8xMe/0LbpuyLDLwmbTwBk=";
     };
 
-	buildInputs = with self; [ colorama ];
+    buildInputs = with self; [ colorama ];
 
-	doCheck = false;
+    doCheck = false;
   };
 
   edalize = buildPythonPackage rec{
     name = "edalize";
     
-	src = pkgs.fetchgit {
-	  url = "https://github.com/lowRISC/edalize.git";
-	  rev = "5ae2c3e1ca306e27d81ce5fcc769f62cb7ac42d0";
-	  sha256 = "sha256-iIf7bUBE2SeS/TByUNL9wI1LswlHTmgHYGJltWXNUWE=";
+    src = pkgs.fetchgit {
+      url = "https://github.com/lowRISC/edalize.git";
+      rev = "5ae2c3e1ca306e27d81ce5fcc769f62cb7ac42d0";
+      sha256 = "sha256-iIf7bUBE2SeS/TByUNL9wI1LswlHTmgHYGJltWXNUWE=";
     };
 
-	nativeBuildInputs = with self; [ jinja2 pytest markupsafe vunit_hdl colorama pyyaml setuptools_scm pandas ];
+    nativeBuildInputs = with self; [ jinja2 pytest markupsafe vunit_hdl colorama pyyaml setuptools_scm pandas ];
     leaveDotGit = true;
   };
 
@@ -105,43 +105,43 @@ let
       leaveDotGit = true;
     };
 
-	doCheck = false;
+    doCheck = false;
   };
 
   okonomiyaki = buildPythonPackage rec{
     name = "okonomiyaki";
-	version = "1.3.1";
+    version = "1.3.1";
 
-	src = pkgs.fetchFromGitHub {
-	  owner = "enthought";
-	  repo = "okonomiyaki";
-	  rev = "df23b5538c8b73585dfcdd00029321f0503fb8bd";
-	  sha256 = "sha256-L4cFQiy2TWPdEfUWImnpA3L8sMGJ/w7DnrJFcA3sgbU=";
-	  fetchSubmodules = true;
-	  leaveDotGit = true;
+    src = pkgs.fetchFromGitHub {
+      owner = "enthought";
+      repo = "okonomiyaki";
+      rev = "df23b5538c8b73585dfcdd00029321f0503fb8bd";
+      sha256 = "sha256-L4cFQiy2TWPdEfUWImnpA3L8sMGJ/w7DnrJFcA3sgbU=";
+      fetchSubmodules = true;
+      leaveDotGit = true;
     };
 
-	buildInputs = with self; [ attrs six jsonschema zipfile2 ];
+    buildInputs = with self; [ attrs six jsonschema zipfile2 ];
 
-	doCheck = false;
+    doCheck = false;
   };
 
   simplesat = buildPythonPackage rec{
     name = "simplesat";
-	version = "0.8.2";
+    version = "0.8.2";
 
-	src = pkgs.fetchFromGitHub {
-	  owner = "enthought";
-	  repo = "sat-solver";
-	  rev = "4ce881644c872e629358e31f37bc8d099ce42591";
-	  sha256 = "sha256-6mYFqXFRLEOVV4yv/BlIVAnvvCksDAVgQFxshYpZUK4=";
-	  fetchSubmodules = true;
-	  leaveDotGit = true;
+    src = pkgs.fetchFromGitHub {
+      owner = "enthought";
+      repo = "sat-solver";
+      rev = "4ce881644c872e629358e31f37bc8d099ce42591";
+      sha256 = "sha256-6mYFqXFRLEOVV4yv/BlIVAnvvCksDAVgQFxshYpZUK4=";
+      fetchSubmodules = true;
+      leaveDotGit = true;
     };
 
-	buildInputs = with self; [ attrs okonomiyaki six jsonschema zipfile2 ];
+    buildInputs = with self; [ attrs okonomiyaki six jsonschema zipfile2 ];
 
-	doCheck = false;
+    doCheck = false;
   };
 
   fusesoc = buildPythonPackage rec{
@@ -149,7 +149,7 @@ let
 
     
     src = pkgs.fetchgit {
-	  url = "https://github.com/lowRISC/fusesoc.git";
+      url = "https://github.com/lowRISC/fusesoc.git";
       rev = "c64ca392382b0c432c893d5ce4a93b77af213fdf";
       sha256 = "sha256-HFrx6vjthNbhrIeV4vNW3YmmqBwqb6lX8lu0dcmQnQM=";
     };
@@ -165,45 +165,45 @@ let
   mypython = (python39.withPackages(ps: with ps; [
     hjson
     libcst
-	Mako
-	pycryptodome
-	pyelftools
-	pytest
-	pytest-timeout
-	pyyaml
-	tabulate
+    Mako
+    pycryptodome
+    pyelftools
+    pytest
+    pytest-timeout
+    pyyaml
+    tabulate
 
-	# dv_sim dependencies
-	# ignored for now
+    # dv_sim dependencies
+    # ignored for now
 
-	# Dependencies: check_dif_statuses.py
+    # Dependencies: check_dif_statuses.py
     pydriller
     termcolor
 
-	# Linters
+    # Linters
     # ignored for now
 
-	# Type stubs for mypy checking.
-	# ignored for now
+    # Type stubs for mypy checking.
+    # ignored for now
 
     # Dependency of sw/host/vendor/google_verible_verilog_syntax_py
-	anytree
+    anytree
 
-	# To be removed with Meson.
+    # To be removed with Meson.
     # Minimum matches version in meson.build
     meson
 
     markupsafe
 
-	# Development version with OT-specific changes
-	okonomiyaki
-	simplesat
-	jinja2
-	fusesoc
-	edalize
+    # Development version with OT-specific changes
+    okonomiyaki
+    simplesat
+    jinja2
+    fusesoc
+    edalize
 
-	# Development version of ChipWhisperer toolchain
-	# ig for now
+    # Development version of ChipWhisperer toolchain
+    # ig for now
   ])).override (args: { ignoreCollisions = true; });
 
 in stdenv.mkDerivation rec {
@@ -214,38 +214,38 @@ in stdenv.mkDerivation rec {
 
   buildInputs = [
     # for dependencies required at run time
-	# usually for a foreign, non-native platform
+    # usually for a foreign, non-native platform
     autoconf
-	bison
-	cmake
-	curl
-	doxygen
-	flex
-	gcc
-	gdb
-	git
-	go
-	lcov
-	libelf
-	libftdi1
+    bison
+    cmake
+    curl
+    doxygen
+    flex
+    gcc
+    gdb
+    git
+    go
+    lcov
+    libelf
+    libftdi1
     ncurses5
-	openssl
-	udev
-	libusb1
-	lsb-release
+    openssl
+    udev
+    libusb1
+    lsb-release
     ninja
-	perl
-	pkgconf
+    perl
+    pkgconf
     mypython
-	python39Packages.pip
-	python39Packages.setuptools
-	python39Packages.wheel
-	srecord
-	tree
-	libxslt
-	zlib
-	xz
-	pkg-config
-	verilator
+    python39Packages.pip
+    python39Packages.setuptools
+    python39Packages.wheel
+    srecord
+    tree
+    libxslt
+    zlib
+    xz
+    pkg-config
+    verilator
   ];
 }
